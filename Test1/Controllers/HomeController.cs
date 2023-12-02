6f6f55710d63f7748dd81b7f515632df59509690
reaksaviriya <vunviriya096@gmail.com>
2023-12-02 10:39:41 +0700

    Product controller

---
+++ Test1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShowInfo()
+        {
+            return View("Show");
+        }
+        public ActionResult Detail()
+        { 
+            return View("Details");
+        }
+        public ActionResult ShowMoreDetail() 
+        {
+            return View("~/Views/About/Index.cshtml");
+        }
+    }
+}
